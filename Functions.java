2983f28315971b40d7568d4e8e340fe9ee445efe
Md. Khairul Anam Shopnil <khairulanamshopnil@gmail.com>
2025-08-04 00:06:30 +0600

    Update Functions.java

--- Functions.java
+++ Functions.java
@@ -1,3 +1,7 @@
+1. Basic Addition Function
+File: SumExample.java    
+Concept: Basic function that takes two integers and returns their sum.
+Code: 
 import java.util.*;
 public class Functions{
     public static int CalculateSum(int num1, int num2){
@@ -12,9 +16,12 @@ public class Functions{
         int sum= CalculateSum(num1,num2);
         System.out.print("Sum is : "+sum);
     }
-} 
+}
 
-
+2. Product Calculation with Multiple Calls
+ File: ProductExample.java   
+Concept: Shows how the same function can be called multiple times with different arguments.
+Code:
 import java.util.*;
 public class Functions{
     public static int CalculateProduct(int a, int b){
@@ -30,47 +37,62 @@ public class Functions{
         int b= sc.nextInt();
         int product= CalculateProduct(a,b);
         System.out.println("Product is: " +product);
-         product=CalculateProduct(10,20);
+        product=CalculateProduct(10,20);
         System.out.println("Product is: " +product);
-
     }
 }
 
+
+3. Complex Function - Binomial Coefficient
+File: BinomialCoefficient.java    
+Concept: Demonstrates:
+                 Function calling another function (bicoeff calls fact)
+                 Mathematical calculations using functions
+                 Code modularity    
+Code:   
 
 import java.util.*;
 public class Functions{
- public static int fact(int n){
-    int f=1;
-    for(int i=1; i<=n; i++){
-        f*=i;
+    public static int fact(int n){
+        int f=1;
+        for(int i=1; i<=n; i++){
+            f*=i;
+        }
+        return f;
     }
-    return f;
+
+    public static int bicoeff(int n, int r){
+        int nfact= fact(n);
+        int rfact= fact(r);
+        int nmrfact= fact(n-r);
+        int bicoeff= nfact/ (rfact*nmrfact);
+        return bicoeff;
+    }
+    
+    public static void main(String args[]){
+        System.out.println(bicoeff(5,2));
+    }
 }
 
-public static int bicoeff(int n, int r){
-    int nfact= fact(n);
-    int rfact= fact(r);
-    int nmrfact= fact(n-r);
-    int bicoeff= nfact/ (rfact*nmrfact);
-    return bicoeff;
-}
-public static void main(String args[]){
-    System.out.println(bicoeff(5,2));
-  }
-}
+4. Function Overloading
+File: FunctionOverloading.java
+Concept: Method Overloading - Same function name with different:
+        Number of parameters
+        Types of parameters
 
+Code: 
 
 import java.util.*;
 public class Functions{
     public static int sum(int a,int b){
         return a+b;
     }
+    
     public static float sum(float a, float b, float c){
         return a+b+c;
     }
 
     public static void main(String args[]){
-        
         System.out.println(sum(5,6));
         System.out.print(sum(40.3f,50.5f,1.5f));
     }
